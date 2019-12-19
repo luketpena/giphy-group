441d5f86fd520f8801c5031e422e8062e0318fb3
Luke Peña <luketpena@gmail.com>
2019-12-19 09:46:52 -0600

    React redux + saga setup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+//Redux + Saga
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {put,takeEvery} from 'redux-saga/effects';
+import axios from 'axios';
+
+function * rootSaga () {
+
+}
+
+
+//Creating store instance for Saga
+const sagaMiddlware = createSagaMiddleware();
+const storeInstance = createStore(
+  combineReducers({
+
+  }),
+  applyMiddleware(sagaMiddlware, logger)
+)
+
+sagaMiddlware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('react-root'));
