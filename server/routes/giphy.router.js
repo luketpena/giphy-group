79eae30fb514594990f725eff4445c03c79196fb
Luke Peña <luketpena@gmail.com>
2019-12-19 10:56:14 -0600

    Made API request and stuff

---
+++ server/routes/giphy.router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const router = express.Router();
+
+router.get('/search/:searchQuery', (req, res) => {
+  let apiKey = process.env.API_KEY;
+  console.log(req.params.searchQuery);
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${req.params.searchQuery}&limit=10`).then(response=>{
+    console.log('FIND BABIES------------>',response.data.data);
+    searchResults = [];
+    for (let item of response.data.data) {
+      searchResults.push({image_url: item.images.original.url, title: item.title})
+    }
+    console.log(searchResults);
+    
+    res.send(searchResults);
+  }).catch(error=>{
+    console.log(error);  
+    res.sendStatus(400);
+  })
+
+});
+
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -11,15 +11,34 @@ import {put,takeEvery} from 'redux-saga/effects';
 import axios from 'axios';
 
 function * rootSaga () {
-
+  yield takeEvery('SEARCH_GIPHY', searchGiphy);
 }
+ // SAGAS
+function * searchGiphy (action) {
+  try{
+    console.log('ACTION PAYLOAD-->',action.payload);
+    const getSearchResponse = yield axios.get(`/api/giphy/search/${action.payload}`);
+    yield put ({ type: 'GET_SEARCH', payload: getSearchResponse.data})
+  }
+  catch (error){
+    console.log('error getting giphy search:', error);
+    
+  }
+}
+  // REDUCERS
+  const searchReducer = (state=[], action) => {
+    if(action.type === 'GET_SEARCH'){
+      return action.payload;
+    }
+    return state;
+  }
 
 
 //Creating store instance for Saga
 const sagaMiddlware = createSagaMiddleware();
-const storeInstance = createStore(
+const storeInstance = createStore (
   combineReducers({
-
+    searchReducer
   }),
   applyMiddleware(sagaMiddlware, logger)
 )
