80d676ea650675f71b74ca4f5579659d1a3457f1
Luke Peña <luketpena@gmail.com>
2019-12-20 12:10:21 -0600

    Brittney is gone.

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -21,6 +21,8 @@ router.post('/', (req, res) => {
   let queryString = `INSERT INTO "favorites" ("title", "image_url") VALUES ($1, $2);`;
 
   pool.query(queryString, [title,image_url]).then(result=>{
+    console.log('ADD RESULT',result);
+    
     res.sendStatus(200);
   }).catch(error=>{
     res.sendStatus(400);
@@ -36,11 +38,20 @@ router.put('/:favId', (req, res) => {
 
 // delete a favorite
 router.delete('/:id', (req, res) => {
-  let queryString = `DELETE FROM "favorites" WHERE "id"=$1;`;
-  pool.query(queryString, [req.params.id]).then(result=>{
-    res.sendStatus(200);
+  //Query Strings
+  let queryString1 = `DELETE FROM "category_favorites" WHERE "favorites_id"=$1;`;
+  let queryString2 = `DELETE FROM "favorites" WHERE "id"=$1;`;
+
+  //Query to dependant tables
+  pool.query(queryString1, [req.params.id]).then(result=>{
+    pool.query(queryString2, [req.params.id]).then(result=>{
+      res.sendStatus(200);
+    }).catch(error=>{
+      console.log('Q2 Error deleting favorites from database:',error);
+      res.sendStatus(400);
+    })
   }).catch(error=>{
-    console.log('Error getting favorites from database:',error);
+    console.log('Q1 Error deleting favorites from database:',error);
     res.sendStatus(400);
   });
 });
