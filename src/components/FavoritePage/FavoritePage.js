6a7aa5b5d6c220340d74cf9942c6b029b4b0be8f
Luke Peña <luketpena@gmail.com>
2019-12-20 09:31:24 -0600

    Merge fix

---
+++ src/components/FavoritePage/FavoritePage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FavoritePage extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Favorite List</h1>
+      </div>
+    )
+  }
+}
+
+export default FavoritePage;
